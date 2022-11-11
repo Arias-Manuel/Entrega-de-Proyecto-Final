8453edbf6c4211e7265043ef631bed9992d6728b
Manuel Alejandro Arias Mercado <arias.mercado.manuel@gmail.com>
2022-11-11 01:51:16 -0300

    Final

--- src/app/UI.js
+++ src/app/UI.js
@@ -1,12 +1,13 @@
 export class UI {
   
-  constructor(){
+  constructor() {
     this.location = document.getElementById('weather-location');
     this.desc = document.getElementById('weather-description');
     this.string = document.getElementById('weather-string');
     this.humidity = document.getElementById('weather-humidity');
     this.wind = document.getElementById('weather-wind');
   }
+
   render (weather) {
     console.log(weather);
     this.location.textContent = weather.name + ' / ' + weather.sys.country;
